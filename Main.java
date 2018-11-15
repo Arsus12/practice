fa4f6541a8962c173cca343c8363ac5d98746607
Arsus <warcrafttral@mail.ru>
2018-11-15 15:44:21 +0700

    laba11111111

--- Main.java
+++ Main.java
@@ -8,17 +8,18 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	Integer n,x=0;
+	Integer n;
+	Long x= Long.valueOf(0);
 	Scanner in = new Scanner(System.in);
 	System.out.print("n=");
 	n = in.nextInt();
 	if(n<0 || n>5){
 	 return;
     }
-	Integer ar[] = new Integer[10];
+	Long ar[] = new Long[10];
 	for (int i = 1; i<=n; i++){
 	    System.out.print("ar"+i+"=");
-	 ar[i] = in.nextInt();
+	 ar[i] = in.nextLong();
 	  x = x + ar[i];
     }
     System.out.println("Result="+x);
